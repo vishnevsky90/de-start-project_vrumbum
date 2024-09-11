1647337a39fe55a0e40ec8c7c321c79d303f4e95
vishnevsky90 <102219542+vishnevsky90@users.noreply.github.com>
2024-09-11 20:02:48 +0300

    Update INITIAL.sql

--- INITIAL.sql
+++ INITIAL.sql
@@ -1,1 +1,2 @@
 /*сохраните в этом файле запросы для первоначальной загрузки данных - создание схемы raw_data и таблицы sales и наполнение их данными из csv файла*/
+\copy raw_date.sales(id,auto,gasoline_consumption,price,date,person_name,phone,discount,brand_origin) FROM C:\Temp\cars.csv CSV HEADER;
