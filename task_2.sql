2e3e0998c7590a8cda695d5adf639c67467cf93f
vishnevsky90 <102219542+vishnevsky90@users.noreply.github.com>
2024-09-12 13:23:20 +0300

    Update task_2.sql

--- task_2.sql
+++ task_2.sql
@@ -1,1 +1,9 @@
 /*добавьте сюда запрос для решения задания 2*/
+SELECT b.brand AS brand_name, extract(YEAR from s.date) AS year, round(avg(s.price),2) AS price_avg
+FROM car_shop.sales s
+INNER JOIN car_shop.auto a 
+	ON s.auto_id =a.id 
+INNER JOIN car_shop.brand b 
+	ON a.brand_id =b.id 
+GROUP BY b.brand, extract(YEAR from s.date)
+ORDER BY b.brand, year
