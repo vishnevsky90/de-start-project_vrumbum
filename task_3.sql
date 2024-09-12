7b208654d9b03c6bf57ecc82e896ba23fd279979
vishnevsky90 <102219542+vishnevsky90@users.noreply.github.com>
2024-09-12 13:23:46 +0300

    Update task_3.sql

--- task_3.sql
+++ task_3.sql
@@ -1,1 +1,6 @@
 /*добавьте сюда запрос для решения задания 3*/
+SELECT extract(MONTH from s.date) AS month, extract(YEAR from s.date) AS year, round(avg(s.price),2) AS price_avg
+FROM car_shop.sales s
+WHERE extract(YEAR from s.date) = 2022
+GROUP BY extract(MONTH from s.date), extract(YEAR from s.date)
+ORDER BY MONTH
