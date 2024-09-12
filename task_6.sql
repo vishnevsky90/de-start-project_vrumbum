bfe1a5a9c673b5ce92003cf13a45b1925f8cd066
vishnevsky90 <102219542+vishnevsky90@users.noreply.github.com>
2024-09-12 13:25:02 +0300

    Update task_6.sql

--- task_6.sql
+++ task_6.sql
@@ -1,1 +1,6 @@
 /*добавьте сюда запрос для решения задания 6*/
+SELECT count(DISTINCT s.client_id) AS persons_from_usa_count
+FROM car_shop.sales s 
+INNER JOIN car_shop.client c 
+	ON s.client_id =c.id 
+WHERE LEFT(c.phone,2) = '+1' 
