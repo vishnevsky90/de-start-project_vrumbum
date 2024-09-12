48917a4e15f5ebc8e736416421f45eb8133c5137
vishnevsky90 <102219542+vishnevsky90@users.noreply.github.com>
2024-09-12 13:22:55 +0300

    Update task_1.sql

--- task_1.sql
+++ task_1.sql
@@ -1,1 +1,6 @@
 /*добавьте сюда запрос для решения задания 1*/
+WITH a AS (SELECT sum(CASE WHEN gasoline_consumption IS NULL THEN 1 ELSE 0	END) AS no_gasoline_consumption ,count(*) AS total
+FROM car_shop.auto)
+
+SELECT cast(no_gasoline_consumption AS decimal)/cast(total AS decimal)*100 AS nulls_percentage_gasoline_consumption
+FROM a
